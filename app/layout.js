50fcb38427bbe0e7c9e0b2841fcc434cb3c29acc
Abhishek-Raj-Poudel <abhishekrajpoudel@gmail.com>
2026-04-19 14:09:49 +0545

    some minor change

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body suppressHydrationWarning>
         <ThemeInitializer />
         {children}
       </body>
